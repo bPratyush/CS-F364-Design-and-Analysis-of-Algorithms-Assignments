8e4285b38e321733c0c37fecabcf8cb2f53712fd
Pratyush Bindal <prathubindal.mzn@gmail.com>
2025-03-08 20:06:38 +0530

    update

--- src/ELS/ELS.cpp
+++ src/ELS/ELS.cpp
@@ -177,11 +177,8 @@ int main(int argc, char* argv[]) {
     outfile << "Total number of maximal cliques: " << totalMaximalCliques << endl;
     outfile << "Execution time (ms): " << duration.count() << endl;
     outfile << "Distribution of different size cliques:" << endl;
-    // Sort the distribution to ensure deterministic order
-    vector<pair<int, int>> sortedDistribution(cliqueSizeDistribution.begin(), cliqueSizeDistribution.end());
-    sort(sortedDistribution.begin(), sortedDistribution.end());
-    for (const auto& pair : sortedDistribution)
-        outfile << "Size " << pair.first << ": " << pair.second << endl;
+    
+    for(const auto& pair : cliqueSizeDistribution) outfile << "Size " << pair.first << ": " << pair.second << endl;
     outfile.close();
     return 0;
 }
